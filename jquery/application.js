e7c6b111723060ca4fb953c2af3caaa2e94eef2d
Tyler Swartz <tgswartz@gmail.com>
2014-05-07 06:55:35 -0700

    edit jquery

--- jquery/application.js
+++ jquery/application.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
 	$('.well-tgs').on('mouseenter',function(){
-		$(this).animate({width:'+=10px'}, 300, funciton(){});
+		$(this).animate({'top': '-10px'});
 	});
 
 });
