fddfdc9f6372697832cf55d80faa34ddc167bc53
Tyler Swartz <tgswartz@gmail.com>
2015-01-02 18:53:08 -0800

    added js for scroll animation

---
+++ jquery.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	$("#scroll-about").click(function(){
+		$('html, body').animate({
+      scrollTop: $(".about-tyler").offset().top
+    }, 1500);
+	});
+
+	$("#scroll-contact").click(function(){
+		$('html, body').animate({
+      scrollTop: $(".get-in-touch").offset().top
+    }, 1500);
+	});
+});
+
